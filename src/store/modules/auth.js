e4e2269ac1f314c00c26f68e73e108a5f056a868
anikisana <younker12345@gmail.com>
2023-03-01 03:10:21 +0600

    Добавляем API запрос на регистрацию

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import authApi from '@/api/auth';
+
 const state = {
     isSubmitting: false
 }
@@ -5,10 +7,33 @@ const state = {
 const mutations = {
     registerStart(state) {
         state.isSubmitting = true;
+    },
+    registerSuccess(state) {
+        state.isSubmitting = false;
+    },
+    registerFailure(state) {
+        state.isSubmitting = false;
+    }
+}
+
+const actions = {
+    register(context, credentials) {
+        return new Promise(resolve => {
+            context.commit("registerStart")
+            authApi.register(credentials)
+                    .then(response => {
+                        context.commit("registerSuccess", response.data.user)
+                        resolve(response.data.user)
+                    })
+                    .catch(result => {
+                        context.commit("registerFailure", result.response.data.errors)
+                    })
+        })
     }
 }
 
 export default {
     state,
-    mutations
+    mutations,
+    actions
 };
