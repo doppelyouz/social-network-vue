8e2ffcd71bdf06269b4c35a26cd460cb499f9cba
anikisana <younker12345@gmail.com>
2023-03-04 03:47:37 +0600

    Создаем верхнее меню

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/register">Register</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <app-topbar />
+    <router-view />
+  </div>
 </template>
 
+<script>
+  import AppTopbar from '@/components/Topbar.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      AppTopbar
+    }
+  };
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
